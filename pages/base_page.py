1179a46f1378805da617a3ba34af487bb86a43a3
ssla0820 <debby870820@gmail.com>
2024-12-06 15:35:43 +0800

    Add new method to basePage class for additional functionality

--- pages/base_page.py
+++ pages/base_page.py
@@ -3,3 +3,5 @@ class basePage():
         self.name = name
     def data(self):
         print(f'calling basePage.data() from {self.name}')
+    def new(self):
+        print(f'add new command in basePage.new() from {self.name}')
